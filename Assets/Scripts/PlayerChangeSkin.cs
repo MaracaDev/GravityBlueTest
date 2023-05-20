7f6f7bbbdc583975948652a6861898f757505e24
Maraca-dev <maraca.dev@gmail.com>
2023-05-20 11:08:42 -0300

    Wear system
    Created a wear system using scriptable

--- Assets/Scripts/PlayerChangeSkin.cs
+++ Assets/Scripts/PlayerChangeSkin.cs
@@ -4,4 +4,30 @@ using UnityEngine;
 
 public class PlayerChangeSkin : MonoBehaviour
 {
+    public SkinScriptable actualSkin;
+    public GameObject skinHead, skinBody;
+
+    public SkinScriptable skinChoosed;
+
+
+    public void Wear(SkinScriptable skin)
+    {
+        skin = actualSkin;
+
+        switch (skin.skinType)
+        {
+            case "Top":
+                skinHead.GetComponent<SpriteRenderer>().sprite = skin.skinArt;
+                break;
+
+            case "Body":
+                skinBody.GetComponent<SpriteRenderer>().sprite = skin.skinArt;
+                break;
+        }
+    }
+
+    private void Start()
+    {
+        Wear(actualSkin);
+    }
 }
---
+++ Assets/Scripts/SkinScriptable.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName ="Skin" , menuName = "SkinScriptable")]
+public class SkinScriptable : ScriptableObject
+{
+    public string skinType;
+    public Sprite skinArt;
+}
+
+
